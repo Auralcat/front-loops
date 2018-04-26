935b8d026a8a4ea0ab9ab4b04aeb3512bbfab5ee
Auralcat <ashleyzt@hi2.in>
2018-04-26 19:39:46 -0300

    Finish creating single select checkmark

--- loop1/step2/assets/scripts/main.js
+++ loop1/step2/assets/scripts/main.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
     // Hide all checkmarks on startup.
     // If you use hide(), the element will be removed.
     $(".check-mark").css("visibility", "hidden");
+
+    // declare lastClicked
+    let lastClicked;
+
     $(".card-item").click(function() {
+        if (lastClicked != undefined) {
+            lastClicked.css("visibility", "hidden");
+        }
         checkMark = $(this).find(".check-mark");
         if (checkMark.css("visibility") === "hidden") {
             checkMark.css("visibility", "visible");
+            lastClicked = checkMark;
         } else {
             checkMark.css("visibility", "hidden");
         }
