8a29a577318c2cc90eccb25936db1e1d573508ab
Auralcat <ashleyzt@hi2.in>
2018-06-08 21:41:16 -0300

    Correct word expansion on hover

--- loop1/step10/scripts/main.js
+++ loop1/step10/scripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.learn-more-container').hover(function() {
         $('.learn-more-line').toggleClass("line-extend");
+        $('.learn-more-container').toggleClass("container-extend");
     });
 });
