370429495bbf56f25f5af09acb81ccf1de0f582d
Auralcat <ashleyzt@hi2.in>
2018-08-19 21:53:46 -0300

    Generate dynamic content

--- elements-course/loop1/step2/scripts/main.js
+++ elements-course/loop1/step2/scripts/main.js
@@ -1,18 +1,12 @@
 let $tabTitles = ['positive', 'negative', 'neutral'].map((id) => document.getElementById(id))
 let $tabContent = document.getElementById('tab-content');
 
-function tabObject (title) {
-    // Stores a tab's title and content string.
-    this.title = title
-    this.content = title + " content"
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
 }
-
-let tabData = [new tabObject('positive'),
-               new tabObject('negative'),
-               new tabObject('neutral')]
 
 for (let i = 0; i < $tabTitles.length; i++) {
     $tabTitles[i].addEventListener('click', function() {
-        $tabContent.innerHTML = tabData[i].content
+        $tabContent.textContent = [capitalize( this.id ), 'content'].join(' ')
     })
 }
