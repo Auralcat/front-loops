8bfba855c8f8739f12501a91570264eac63fd759
Auralcat <ashleyzt@hi2.in>
2018-08-23 14:30:22 -0300

    Change switches' state randomly

--- elements-course/loop1/step3/scripts/main.js
+++ elements-course/loop1/step3/scripts/main.js
@@ -1,6 +1,17 @@
-let $switchList = document.getElementsByClassName('switch')
+let $switchList = [1, 2, 3, 4].map((n) => document.getElementById('switch' + n))
+
+function randomToggleSwitch ($singleSwitch) {
+    let trurl = Math.floor(Math.random() * 100)
+    if (trurl < 50) {
+        $singleSwitch.classList.toggle("switch__on")
+    }
+}
+
 for (let i = 0; i < $switchList.length; i++) {
     $switchList[i].addEventListener('click', function() {
-        $switchList[i].classList.toggle("switch__on")
+        let $otherSwitches = [...$switchList]
+        $otherSwitches.splice(i, 1)
+        $otherSwitches.forEach(randomToggleSwitch)
+        this.classList.toggle("switch__on")
     })
 }
